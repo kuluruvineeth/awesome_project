eedaf759dad65769b6953c0f115d3e5ea9bf7d36
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-04-22 03:20:30 -0700

    ch3 rows and columns

--- lib/main.dart
+++ lib/main.dart
@@ -16,32 +16,36 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('AgriRise - Way to new Revolution')
       ),
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(8),
-          width: 100,
-          height: 100,
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            color: Colors.red,
-            borderRadius: BorderRadius.circular(10),
-            gradient: LinearGradient(colors: [
-              Colors.pink,
-              Colors.yellow
-            ]),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey,
-                blurRadius: 10
-              )
-            ]
-          ),
-          child: Text('I am new Agri App',textAlign: TextAlign.center,style: TextStyle(
-            fontSize: 20,
-            color: Colors.white,
-            fontWeight: FontWeight.bold
-          )),
-        ),
+      body: Container(
+        width: 300,
+        color: Colors.teal,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(8),
+              width: 100,
+              height: 100,
+              color: Colors.red,
+              alignment: Alignment.center,
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              width: 100,
+              height: 100,
+              color: Colors.yellow,
+              alignment: Alignment.center,
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              width: 100,
+              height: 100,
+              color: Colors.green,
+              alignment: Alignment.center,
+            )
+          ],
+        )
       )
     );
   }
